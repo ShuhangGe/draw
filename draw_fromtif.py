71a6e7019bd2d7b1618eb6da4f09de9707343401
ShuhangGe <sg7484@nyu.edu>
2024-06-19 21:22:29 -0400

    add

---
+++ draw_fromtif.py
@@ -0,0 +1,3 @@
+data_path = '/media/shuhang/T8/projects/RA/dalian/evaluate'
+path2 ='_30_dalian_x6400_y1280.tif_out.txt'
+tif_path = '/media/shuhang/T8/projects/draw/feature_dalian_x13568_y7680.tif'
